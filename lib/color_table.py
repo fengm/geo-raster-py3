3755cf9c534e2f8dd8b705c4c9568d0ecfd42216
fengm <feng.tank@gmail.com>
2019-10-16 17:13:22 +0000

    fixed a bug

--- lib/color_table.py
+++ lib/color_table.py
@@ -76,6 +76,9 @@ class color_table:
         _colors = {}
         with open(f) as _fi:
             _ls = _fi.read().strip().splitlines()
+            if len(_ls) == 0:
+                raise Exception('color table is empty')
+
             if 'QGIS' in _ls[0]:
                 return self._load_qgis_colors(_ls)
                 
