15b008523762c175d324554a90e44d3375c5f3e2
fengm <feng.tank@gmail.com>
2020-06-27 07:40:30 +0000

    add requirements.txt -- but it does not work

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ for _root, _dirs, _files in os.walk('util'):
 		_f = os.path.join(_root, _file)
 		_ss.append(_f)
 
-setuptools.setup(name='geo-raster', version='1.0', description='', \
+setuptools.setup(name='geo-raster', version='1.0.1', description='', \
 		author='Min Feng', author_email='mfeng.geo@gmail.com', \
 		# packages=['gio', 'gio/data/landsat'],
 		# package_dir={'gio': 'lib', 'gio/data/landsat': 'lib/data/landsat'},
@@ -39,5 +39,6 @@ setuptools.setup(name='geo-raster', version='1.0', description='', \
 		cmdclass = {"build_ext": build_ext},
 		ext_modules=_ms,
 		scripts=_ss,
+		install_requires=['cython']
 		)
 
